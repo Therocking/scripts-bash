bcc060f1da1627ba035afa604b46b741a92102e5
jose <jgenao588@gmail.com>
2024-01-09 20:27:59 -0400

    new script

---
+++ dropbox_sync.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+# Scritp para sincronizar archivos de la maquina en dropbox
+
+dropbox start
+
+cp ~/listaInstalaciones.* ~/Dropbox/linux-lite/2023-10/
+
+dropbox stop
--- push_practica-codigo.sh
+++ push_practica-codigo.sh
@@ -1,10 +1,9 @@
-#/bin/zsh
+#!/bin/zsh
 
 # Script para automatizar el pusheo de todo lo que he practicado en la semana a githup.
 
-#cd ~/practicar-codigo
-cd ~/scripts
+cd ~/practicar-codigo
 
 git add .
 git commit -m "$(date +"%d-%m-%Y %X")"
-#git push origin main
+git push -u origin main
