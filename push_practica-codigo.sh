d23028a1dbcee8950866b9057920894894190a07
Jose <jgenao588@gmail.com>
2024-02-04 12:53:58 -0400

    script to push modified

--- push_practica-codigo.sh
+++ push_practica-codigo.sh
@@ -2,7 +2,7 @@
 
 # Script para automatizar el pusheo de todo lo que he practicado en la semana a githup.
 
-cd ~/practicar-codigo
+cd ~/practica-codigo
 
 git add .
 git commit -m "$(date +"%d-%m-%Y %X")"
